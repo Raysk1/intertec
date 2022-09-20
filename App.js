a29da3a87487f7e87dd68f50396381a36a693558
Raysk1 <francogarciaoscar@gmail.com>
2022-09-19 20:11:58 -0600

    *Se cambio la UI para que fuera responsiva *limpieza de codigo

--- App.js
+++ App.js
@@ -3,25 +3,28 @@ import * as React from "react";
 import InicioDeSesion from "./components/InicioDeSesion";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from './components/Home'
+import Home from "./components/Home";
+import { SafeAreaView } from "react-native";
 
 function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="InicioDeSesion">
-        <Stack.Screen
-          name="InicioDeSesion"
-          component={InicioDeSesion}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="InicioDeSesion">
+          <Stack.Screen
+            name="InicioDeSesion"
+            component={InicioDeSesion}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
